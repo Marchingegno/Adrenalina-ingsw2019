4b566a827c8ad36507ca0401922f2e72eb4b69df
Desno365 <desno365@gmail.com>
2019-04-09 17:40:14 +0200

    Add JavaDoc to AmmoContainer

--- src/main/java/it/polimi/se2019/model/cards/ammo/AmmoContainer.java
+++ src/main/java/it/polimi/se2019/model/cards/ammo/AmmoContainer.java
@@ -1,12 +1,20 @@
 package it.polimi.se2019.model.cards.ammo;
 
+/**
+ * Contains all the ammos of the player.
+ * @author Desno365
+ */
 public class AmmoContainer {
 
 	public static final int MAX_AMMO = 3;
 	public static final int INITIAL_AMMO = 1;
+
 	private int[] ammos;
 
 
+	/**
+	 * Create the container with initial values of ammos.
+	 */
 	public AmmoContainer() {
 		ammos = new int[AmmoType.values().length];
 		for (AmmoType ammoType : AmmoType.values()) {
@@ -15,10 +23,20 @@ public class AmmoContainer {
 	}
 
 
+	/**
+	 * Get the amount of ammos given the type of ammo.
+	 * @param ammoType type of the ammo.
+	 * @return number of ammos.
+	 */
 	public int getAmmo(AmmoType ammoType) {
 		return ammos[ammoType.ordinal()];
 	}
 
+	/**
+	 * Add ammos per ammo type.
+	 * @param ammoToAdd type of ammo that is added.
+	 * @param numOfAmmoToAdd number of ammo added, must be >= 0.
+	 */
 	public void addAmmo(AmmoType ammoToAdd, int numOfAmmoToAdd) {
 		if(numOfAmmoToAdd < 0)
 			throw new IllegalArgumentException("numOfAmmoToAdd cannot be negative.");
@@ -29,6 +47,19 @@ public class AmmoContainer {
 			ammos[ammoToAdd.ordinal()] = MAX_AMMO;
 	}
 
+	/**
+	 * Add a single ammo of ammo type.
+	 * @param ammoToAdd type of ammo that is added.
+	 */
+	public void addAmmo(AmmoType ammoToAdd) {
+		addAmmo(ammoToAdd, 1);
+	}
+
+	/**
+	 * Remove ammos per ammo type.
+	 * @param ammoToRemove type of ammo that is removed.
+	 * @param numOfAmmoToRemove number of ammo removed, must be >= 0.
+	 */
 	public void removeAmmo(AmmoType ammoToRemove, int numOfAmmoToRemove) {
 		if(numOfAmmoToRemove < 0)
 			throw new IllegalArgumentException("numOfAmmoToRemove cannot be negative.");
