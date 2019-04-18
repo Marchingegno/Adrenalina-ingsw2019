93241e6a2eab9c8d162bd4ef334cf232353a4cb8
Desno365 <desno365@gmail.com>
2019-04-18 11:47:44 +0200

    Add methods in Utils to manage colors in the CLI

--- src/main/java/it/polimi/se2019/utils/Utils.java
+++ src/main/java/it/polimi/se2019/utils/Utils.java
@@ -25,12 +25,48 @@ public class Utils {
 	}
 
 	public static void logInfo(String msg) {
-		System.out.println("INFO: " + msg);
+		System.out.println(setColorString(31, 44) + "INFO:" + resetColorString() + " " + msg);
 		//LOGGER.log(Level.INFO, msg);
 	}
 
 	public static Logger getGlobalLogger() {
 		return LOGGER;
+	}
+
+	private static String setColorString(int characterColor, int backgroundColor) {
+		if(characterColor < 30 || characterColor > 39)
+			throw new IllegalArgumentException("Character color must be between 30 and 39.");
+		if(backgroundColor < 40 || backgroundColor > 49)
+			throw new IllegalArgumentException("Background color must be between 40 and 49.");
+		/*
+		+~~~~~~+~~~~~~+~~~~~~~~~~~~~~~~~~~~~+
+		|  fg  |  bg  |  color              |
+		+~~~~~~+~~~~~~+~~~~~~~~~~~~~~~~~~~~~+
+		|  30  |  40  |  black or white (!) |
+		|  31  |  41  |  red                |
+		|  32  |  42  |  green              |
+		|  33  |  43  |  yellow             |
+		|  34  |  44  |  blue               |
+		|  35  |  45  |  magenta            |
+		|  36  |  46  |  cyan               |
+		|  37  |  47  |  white or grey      |
+		|  39  |  49  |  default            |
+		+~~~~~~+~~~~~~+~~~~~~~~~~~~~~~~~~~~~+
+		*/
+		return (char)27 + "[" + characterColor + ";" + backgroundColor + "m";
+	}
+
+	private static String resetColorString() {
+		return setColorString(39, 49);
+	}
+	
+	private static void testColors() {
+		for (int i = 30; i <= 39; i++) {
+			for (int j = 40; j <= 49; j++) {
+				System.out.print(setColorString(i, j) + " TEST " + resetColorString());
+			}
+			System.out.print("\n");
+		}
 	}
 
 }
