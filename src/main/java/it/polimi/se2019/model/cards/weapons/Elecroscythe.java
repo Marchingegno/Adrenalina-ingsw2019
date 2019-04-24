ec1943ed8e4ebb51372447563089a5b9ea1eae74
Matteo Marchisciana <24322895+Marchingegno@users.noreply.github.com>
2019-04-24 18:50:41 +0200

    Update

--- src/main/java/it/polimi/se2019/model/cards/weapons/Elecroscythe.java
+++ src/main/java/it/polimi/se2019/model/cards/weapons/Elecroscythe.java
@@ -4,12 +4,21 @@ package it.polimi.se2019.model.cards.weapons;
 import it.polimi.se2019.model.cards.ammo.AmmoType;
 import it.polimi.se2019.model.player.Player;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public final class Elecroscythe extends AlternateFire {
+	private final int PRIMARY_DAMAGE = 1;
+	private final int PRIMARY_MARKS = 0;
+	private final int SECONDARY_DAMAGE = 2;
+	private final int SECONDARY_MARKS = 0;
+
 
 	public Elecroscythe(String description, List<AmmoType> reloadPrice) {
 		super(description, reloadPrice);
+		this.standardDamagesAndMarks = new ArrayList<>();
+		this.standardDamagesAndMarks.add(new DamageAndMarks(PRIMARY_DAMAGE, PRIMARY_MARKS));
+		this.standardDamagesAndMarks.add(new DamageAndMarks(SECONDARY_DAMAGE, SECONDARY_MARKS));
 	}
 
 
@@ -25,11 +34,12 @@ public final class Elecroscythe extends AlternateFire {
 
 	@Override
 	public List<Player> getPrimaryTargets() {
+		//Get player in the same square
 		return null;
 	}
 
 	@Override
 	public List<Player> getSecondaryTargets() {
-		return null;
+		return getPrimaryTargets();
 	}
 }
--- src/main/java/it/polimi/se2019/model/cards/weapons/FlameThrower.java
+++ src/main/java/it/polimi/se2019/model/cards/weapons/FlameThrower.java
@@ -1,13 +1,27 @@
 package it.polimi.se2019.model.cards.weapons;
 
+import com.sun.org.apache.bcel.internal.generic.ARRAYLENGTH;
+import it.polimi.se2019.model.cards.ammo.AmmoType;
 import it.polimi.se2019.model.player.Player;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public final class FlameThrower extends AlternateFire {
+	private final int PRIMARY_DAMAGE = 1;
+	private final int PRIMARY_MARKS = 0;
+	private final int SECONDARY_DAMAGE = 2;
+	private final int SECONDARY_MARKS = 0;
+	private ArrayList<DamageAndMarks> secondaryDamagesAndMarks;
 
 	public FlameThrower(String description, ArrayList<AmmoType> reloadPrice) {
 		super(description, reloadPrice);
+		this.standardDamagesAndMarks = new ArrayList<>();
+		this.standardDamagesAndMarks.add(new DamageAndMarks(PRIMARY_DAMAGE, PRIMARY_MARKS));
+		this.standardDamagesAndMarks.add(new DamageAndMarks(SECONDARY_DAMAGE, SECONDARY_MARKS));
+		secondaryDamagesAndMarks = new ArrayList<>();
+		secondaryDamagesAndMarks.add(new DamageAndMarks(SECONDARY_DAMAGE, SECONDARY_MARKS));
+		secondaryDamagesAndMarks.add(new DamageAndMarks(PRIMARY_DAMAGE, PRIMARY_MARKS));
 	}
 
 
@@ -15,6 +29,7 @@ public final class FlameThrower extends AlternateFire {
 	}
 
 	public List<Player> secondaryFire() {
+
 	}
 
 
