391c848fd34aee3e9a903b5a9fe1bcd84f789424
Desno365 <desno365@gmail.com>
2019-05-01 16:20:36 +0200

    Add comments in MessageReceiverInterface

--- src/main/java/it/polimi/se2019/network/client/MessageReceiverInterface.java
+++ src/main/java/it/polimi/se2019/network/client/MessageReceiverInterface.java
@@ -4,7 +4,14 @@ import it.polimi.se2019.network.message.Message;
 
 public interface MessageReceiverInterface {
 
+	/**
+	 * Receive and process the message sent by the server both by socket or by RMI.
+	 * @param message the message received.
+	 */
 	void processMessage(Message message);
 
+	/**
+	 * Called when the connection with the server (by socket or by RMI) has been lost.
+	 */
 	void lostConnection();
 }
