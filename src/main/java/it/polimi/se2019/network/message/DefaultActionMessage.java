5dade3e1c72038464ba29ba82724e84905cbd1de
Desno365 <desno365@gmail.com>
2019-05-09 12:10:20 +0200

    DefaultActionMessage can use existing IntMessage

--- src/main/java/it/polimi/se2019/network/message/DefaultActionMessage.java
+++ src/main/java/it/polimi/se2019/network/message/DefaultActionMessage.java
@@ -4,15 +4,9 @@ package it.polimi.se2019.network.message;
  * Used for action requesting, grab, spawn, shoot and reload.
  * @author Marchingegno
  */
-	private int index;
 public class DefaultActionMessage extends IntMessage {
 
 	public DefaultActionMessage(int index, MessageType messageType, MessageSubtype messageSubtype) {
-		super(messageType, messageSubtype);
-		this.index = index;
-	}
-
-	public int getIndex() {
-		return index;
+		super(index, messageType, messageSubtype);
 	}
 }
