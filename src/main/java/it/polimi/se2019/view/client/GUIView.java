c4959f81004bd19bd2cc54e04ded617a74c5372c
Desno365 <desno365@gmail.com>
2019-05-09 11:01:28 +0200

    More GUI code

--- src/main/java/it/polimi/se2019/view/client/GUIView.java
+++ src/main/java/it/polimi/se2019/view/client/GUIView.java
@@ -4,6 +4,7 @@ import it.polimi.se2019.model.gameboard.GameBoardRep;
 import it.polimi.se2019.model.gamemap.GameMapRep;
 import it.polimi.se2019.model.player.PlayerRep;
 import it.polimi.se2019.network.client.Client;
+import it.polimi.se2019.network.message.GameConfigMessage;
 import it.polimi.se2019.network.message.MessageSubtype;
 import it.polimi.se2019.network.message.NicknameMessage;
 import it.polimi.se2019.utils.GameConstants;
@@ -14,7 +15,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.text.DecimalFormat;
 import java.util.List;
-import java.util.Locale;
 
 public class GUIView extends RemoteView {
 
@@ -22,10 +22,11 @@ public class GUIView extends RemoteView {
 	private JFrame waitingRoomFrame;
 	private JLabel waitingPlayersTextWaitingRoom;
 	private JLabel timerTextWaitingRoom;
+	private JFrame gameConfigFrame;
+	private JDialog gameConfigWaitingDialog;
 
 	public GUIView() {
 		this.modelRep = new ModelRep();
-		JComponent.setDefaultLocale(Locale.ENGLISH);
 	}
 
 	@Override
@@ -122,12 +123,19 @@ public class GUIView extends RemoteView {
 
 	@Override
 	public void askMapAndSkullsToUse() {
-
+		closeWaitingRoomFrame();
+		showGameConfigFrame();
 	}
 
 	@Override
 	public void showMapAndSkullsInUse(int skulls, GameConstants.MapType mapType) {
+		closeGameConfigFrame();
+		closeGameConfigWaiting();
 
+		String message = "Average of voted skulls: " + skulls + ".\n" +
+				"Most voted map: " + mapType.getDescription() + "\n\n" +
+				"Match ready to start!";
+		JOptionPane.showMessageDialog(null, message, "Starting Match...", JOptionPane.INFORMATION_MESSAGE);
 	}
 
 	// TODO remove
@@ -192,6 +200,8 @@ public class GUIView extends RemoteView {
 	}
 
 
+	// Not used, here just for example
+	// TODO remove
 	private void dialogWithCancel() {
 		Object[] possibleValues = { "RMI", "Socket" };
 		Object selectedValue = JOptionPane.showInputDialog(null,
@@ -217,28 +227,113 @@ public class GUIView extends RemoteView {
 
 		// Create Panel.
 		JPanel panel = new JPanel();
-		panel.setLayout(new GridLayout(3, 1));
+		GridLayout gridLayout = new GridLayout(0, 1); // With 0 rows is like a vertical layout.
+		gridLayout.setHgap(20);
+		panel.setLayout(gridLayout);
 
 		JLabel introductionText = new JLabel("Waiting for other players to connect.");
-		introductionText.setVerticalTextPosition(JLabel.CENTER);
-		introductionText.setHorizontalTextPosition(JLabel.CENTER);
 		introductionText.setOpaque(true);
 		panel.add(introductionText);
 
 		waitingPlayersTextWaitingRoom = new JLabel("Players in the waiting room: .");
-		waitingPlayersTextWaitingRoom.setVerticalTextPosition(JLabel.CENTER);
-		waitingPlayersTextWaitingRoom.setHorizontalTextPosition(JLabel.CENTER);
 		waitingPlayersTextWaitingRoom.setOpaque(true);
 		panel.add(waitingPlayersTextWaitingRoom);
 
-		timerTextWaitingRoom = new JLabel("");
-		timerTextWaitingRoom.setVerticalTextPosition(JLabel.CENTER);
-		timerTextWaitingRoom.setHorizontalTextPosition(JLabel.CENTER);
+		timerTextWaitingRoom = new JLabel(" ");
 		timerTextWaitingRoom.setOpaque(true);
 		panel.add(timerTextWaitingRoom);
 
 		waitingRoomFrame.add(panel);
 		waitingRoomFrame.pack(); // Sizes the frame so that all its contents are at or above their preferred sizes.
 		waitingRoomFrame.setVisible(true); // Set the window to be visible.
+	}
+
+	private void closeWaitingRoomFrame() {
+		if(waitingRoomFrame != null) {
+			waitingRoomFrame.setVisible(false);
+			waitingRoomFrame.dispose();
+		}
+	}
+
+	private void showGameConfigFrame() {
+		// Create options.
+		String[] skullsOptions = new String[GameConstants.MAX_SKULLS - GameConstants.MIN_SKULLS + 1];
+		for (int i = GameConstants.MIN_SKULLS; i <= GameConstants.MAX_SKULLS; i++)
+			skullsOptions[i - GameConstants.MIN_SKULLS] = ((Integer) i).toString();
+		String[] mapOptions = new String[GameConstants.MapType.values().length];
+		for (int i = 0; i < GameConstants.MapType.values().length; i++) {
+			mapOptions[i] = GameConstants.MapType.values()[i].getDescription();
+		}
+
+		// Create frame.
+		gameConfigFrame = new JFrame("Game Configurations");
+		gameConfigFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		// Up panel.
+		final JPanel upPanel = new JPanel();
+		GridLayout upGridLayout = new GridLayout(1,1);
+		upPanel.setLayout(upGridLayout);
+		JLabel introductionText = new JLabel("Select the map and how many skulls you would like to use.");
+		upPanel.add(introductionText);
+
+		// Down panel.
+		JPanel downPanel = new JPanel();
+		downPanel.setLayout(new GridLayout(2,3));
+		downPanel.add(new JLabel("Skulls:"));
+		downPanel.add(new JLabel("Map:"));
+		downPanel.add(new JLabel(" "));
+		JComboBox skullsComboBox = new JComboBox(skullsOptions);
+		downPanel.add(skullsComboBox);
+		JComboBox mapComboBox = new JComboBox(mapOptions);
+		mapComboBox.setPreferredSize(skullsComboBox.getPreferredSize()); // Make the same dimension as the skulls options.
+		downPanel.add(mapComboBox);
+		JButton doneButton = new JButton("Done");
+		doneButton.addActionListener(e -> {
+			closeGameConfigFrame();
+			showGameConfigWaiting();
+
+			int skulls = skullsComboBox.getSelectedIndex() + GameConstants.MIN_SKULLS;
+			int mapIndex = mapComboBox.getSelectedIndex();
+
+			GameConfigMessage gameConfigMessage = new GameConfigMessage(MessageSubtype.ANSWER);
+			gameConfigMessage.setMapIndex(mapIndex);
+			gameConfigMessage.setSkulls(skulls);
+			sendMessage(gameConfigMessage);
+		});
+		downPanel.add(doneButton);
+
+		// Add panels to container panel.
+		gameConfigFrame.getContentPane().add(upPanel, BorderLayout.NORTH);
+		gameConfigFrame.getContentPane().add(new JSeparator(), BorderLayout.CENTER);
+		gameConfigFrame.getContentPane().add(downPanel, BorderLayout.SOUTH);
+
+		// Display the window.
+		gameConfigFrame.pack();
+		gameConfigFrame.setVisible(true);
+	}
+
+	private void closeGameConfigFrame() {
+		if(gameConfigFrame != null) {
+			gameConfigFrame.setVisible(false);
+			gameConfigFrame.dispose();
+		}
+	}
+
+	private void showGameConfigWaiting() {
+		gameConfigWaitingDialog = new JDialog();
+		gameConfigWaitingDialog.setTitle("Waiting for other clients...");
+
+		JLabel label = new JLabel("Waiting for other clients to answer...");
+
+		gameConfigWaitingDialog.add(label);
+		gameConfigWaitingDialog.pack();
+		gameConfigWaitingDialog.setVisible(true);
+	}
+
+	private void closeGameConfigWaiting() {
+		if(gameConfigWaitingDialog != null) {
+			gameConfigWaitingDialog.setVisible(false);
+			gameConfigWaitingDialog.dispose();
+		}
 	}
 }
