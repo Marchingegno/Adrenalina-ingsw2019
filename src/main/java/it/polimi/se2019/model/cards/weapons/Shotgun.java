6bcc8af1040ec45e97dfbafdc9c9b0f846a4d4fe
Matteo Marchisciana <24322895+Marchingegno@users.noreply.github.com>
2019-05-15 20:31:28 +0200

    Initiate skeleton of shotgun

--- src/main/java/it/polimi/se2019/model/cards/weapons/Shotgun.java
+++ src/main/java/it/polimi/se2019/model/cards/weapons/Shotgun.java
@@ -1,5 +1,12 @@
 package it.polimi.se2019.model.cards.weapons;
 
+import it.polimi.se2019.model.cards.ammo.AmmoType;
+import it.polimi.se2019.model.player.Player;
+import it.polimi.se2019.utils.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Shotgun extends AlternateFire {
 
 	public Shotgun(String description, ArrayList<AmmoType> reloadPrice) {
@@ -7,10 +14,30 @@ public class Shotgun extends AlternateFire {
 	}
 
 
+	@Override
+	Pair handlePrimaryFire(int choice) {
+		return null;
+	}
+
+	@Override
+	Pair handleSecondaryFire(int choice) {
+		return null;
+	}
+
 	public void primaryFire() {
 	}
 
 	public void secondaryFire() {
+	}
+
+	@Override
+	public List<Player> getPrimaryTargets() {
+		return null;
+	}
+
+	@Override
+	public List<Player> getSecondaryTargets() {
+		return null;
 	}
 
 
