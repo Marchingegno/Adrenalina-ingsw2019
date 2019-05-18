d68ff49f0a33846884e64813477e91cc7418dd7d
andreamarcer <marcer.andrea@gmail.com>
2019-05-18 10:49:07 +0200

    start populating weapon deck

--- src/main/java/it/polimi/se2019/model/cards/weapons/WeaponDeck.java
+++ src/main/java/it/polimi/se2019/model/cards/weapons/WeaponDeck.java
@@ -2,12 +2,53 @@ package it.polimi.se2019.model.cards.weapons;
 
 import it.polimi.se2019.model.cards.Card;
 import it.polimi.se2019.model.cards.Deck;
+import it.polimi.se2019.model.cards.ammo.AmmoType;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class WeaponDeck extends Deck<Card> {
 
 	public void initializeDeck(){
-		for (int i = 0; i < 40; i++) {
-			addCard(new Cyberblade("Hello boy, this cyberblade is very nice"));
+
+		for (int i = 0; i < 3; i++) {
+			List<AmmoType> price = new ArrayList<>();
+			price.add(AmmoType.BLUE_AMMO);
+			addCard(new Elecroscythe("placeHolder", price));
+
+			price = new ArrayList<>();
+			price.add(AmmoType.RED_AMMO);
+			price.add(AmmoType.BLUE_AMMO);
+			addCard(new Furnace("placeHolder", price));
+
+			price = new ArrayList<>();
+			price.add(AmmoType.RED_AMMO);
+			price.add(AmmoType.YELLOW_AMMO);
+			addCard(new Hellion("placeHolder", price));
+
+			price = new ArrayList<>();
+			price.add(AmmoType.YELLOW_AMMO);
+			addCard(new Shockwave("placeHolder", price));
+
+			price = new ArrayList<>();
+			price.add(AmmoType.YELLOW_AMMO);
+			price.add(AmmoType.YELLOW_AMMO);
+			addCard(new Shotgun("placeHolder", price));
+
+			price = new ArrayList<>();
+			price.add(AmmoType.BLUE_AMMO);
+			addCard(new TractorBeam("placeHolder", price));
+
+			price = new ArrayList<>();
+			price.add(AmmoType.BLUE_AMMO);
+			price.add(AmmoType.BLUE_AMMO);
+			price.add(AmmoType.YELLOW_AMMO);
+			addCard(new Whisper("placeHolder", price));
+
+			price = new ArrayList<>();
+			price.add(AmmoType.YELLOW_AMMO);
+			price.add(AmmoType.RED_AMMO);
+			addCard(new ZX_2("placeHolder", price));
 		}
 	}
 
