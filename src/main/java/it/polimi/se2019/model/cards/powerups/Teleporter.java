b5e6b0f3e692528aa33734f7d74a6d33f13bdffb
Desno365 <desno365@gmail.com>
2019-05-20 21:13:52 +0200

    Implement Teleporter

--- src/main/java/it/polimi/se2019/model/cards/powerups/Teleporter.java
+++ src/main/java/it/polimi/se2019/model/cards/powerups/Teleporter.java
@@ -1,7 +1,12 @@
 package it.polimi.se2019.model.cards.powerups;
 
 import it.polimi.se2019.model.cards.ammo.AmmoType;
+import it.polimi.se2019.model.gamemap.Coordinates;
+import it.polimi.se2019.network.message.CoordinatesAnswerMessage;
 import it.polimi.se2019.network.message.Message;
+import it.polimi.se2019.utils.Utils;
+
+import java.util.List;
 
 /**
  * This class implements the Teleporter powerup
@@ -10,6 +15,8 @@ import it.polimi.se2019.network.message.Message;
  * @author Desno365
  */
 public class Teleporter extends PowerupCard {
+
+	private int progress = 0;
 
 	private static final String DESCRIPTION =
 			"You may play this card on your turn before\n" +
@@ -26,9 +33,17 @@ public class Teleporter extends PowerupCard {
 
 	@Override
 	public PowerupInfo doPowerupStep(Message answer) {
-		// TODO ask client where to move (can be moved anywhere).
-		// TODO move activatingPlayer.
-		return null;
+		progress++;
+		if(progress == 1) {
+			return firstStep();
+		} else if(progress == 2) {
+			progress = 0;
+			lastStep(answer);
+			return null;
+		} else {
+			Utils.logError("Wrong progress.", new IllegalStateException());
+			return null;
+		}
 	}
 
 	/**
@@ -43,6 +58,25 @@ public class Teleporter extends PowerupCard {
 	@Override
 	public String toString() {
 		return "Teleporter";
+	}
+
+
+	private PowerupInfo firstStep() {
+		Coordinates start = getGameBoard().getGameMap().getPlayerCoordinates(getOwnerPlayer());
+		List<Coordinates> allCoordinates = getGameBoard().getGameMap().reachableCoordinates(start, 10);
+
+		PowerupInfo powerupInfo = new PowerupInfo();
+		powerupInfo.setAskCoordinates("Choose where to move.", allCoordinates);
+		return powerupInfo;
+	}
+
+	private void lastStep(Message answer) {
+		Coordinates targetCoordinates = ((CoordinatesAnswerMessage) answer).getSingleCoordinates();
+		if(!targetCoordinates.equals(getGameBoard().getGameMap().getPlayerCoordinates(getOwnerPlayer()))) {
+			getGameBoard().getGameMap().movePlayerTo(getOwnerPlayer(), targetCoordinates);
+			getGameBoard().getGameMap().updateRep();
+			getGameBoard().getGameMap().notifyObservers();
+		}
 	}
 
 }
