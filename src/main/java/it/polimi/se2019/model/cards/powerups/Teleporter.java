8fb88047dbb3d731de4a3abd33751708681c2b07
Desno365 <desno365@gmail.com>
2019-05-21 17:07:36 +0200

    Fix Teleporter allowing teleportation on the current square

--- src/main/java/it/polimi/se2019/model/cards/powerups/Teleporter.java
+++ src/main/java/it/polimi/se2019/model/cards/powerups/Teleporter.java
@@ -65,6 +65,7 @@ public class Teleporter extends PowerupCard {
 	private QuestionContainer firstStep() {
 		Coordinates start = getGameBoard().getGameMap().getPlayerCoordinates(getOwnerPlayer());
 		List<Coordinates> allCoordinates = getGameBoard().getGameMap().reachableCoordinates(start, 10);
+		allCoordinates.remove(start);
 
 		return QuestionContainer.createCoordinatesQuestionContainer("Choose where to move.", allCoordinates);
 	}
