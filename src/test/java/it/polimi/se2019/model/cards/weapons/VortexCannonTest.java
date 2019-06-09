3f57e5d93d21ced9c7c699100d538f67af4c7f45
Matteo Marchisciana <24322895+Marchingegno@users.noreply.github.com>
2019-06-09 23:10:32 +0200

    Begin VortexCannonTest, maintaining the workaround.

--- src/test/java/it/polimi/se2019/model/cards/weapons/VortexCannonTest.java
+++ src/test/java/it/polimi/se2019/model/cards/weapons/VortexCannonTest.java
@@ -4,16 +4,20 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import it.polimi.se2019.model.Model;
+import it.polimi.se2019.model.gamemap.Coordinates;
 import it.polimi.se2019.model.gamemap.GameMap;
 import it.polimi.se2019.model.player.Player;
 import it.polimi.se2019.utils.GameConstants;
-import org.junit.Test;
+import it.polimi.se2019.utils.QuestionContainer;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
 
 public class VortexCannonTest {
 	private WeaponCard vortexCannon;
@@ -23,6 +27,7 @@ public class VortexCannonTest {
 
 	//@Before
 	public void setUp() throws Exception {
+		//The first player is the shooter.
 		Reader reader = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/decks/Weapon.json")));
 		JsonArray weapons = new JsonParser().parse(reader).getAsJsonArray();
 		for (JsonElement weapon : weapons) {
@@ -37,30 +42,26 @@ public class VortexCannonTest {
 			playerNicknames.add("Player " + i);
 		}
 
-		model = new Model(GameConstants.MapType.MEDIUM_MAP.getMapName(), playerNicknames, 8);
+		model = new Model(GameConstants.MapType.SMALL_MAP.getMapName(), playerNicknames, 8);
+
+		gameMap = model.getGameBoard().getGameMap();
+		players = model.getGameBoard().getPlayers();
+		gameMap.movePlayerTo(players.get(0), new Coordinates(2, 1));
+		gameMap.movePlayerTo(players.get(1), new Coordinates(1, 1));
+		gameMap.movePlayerTo(players.get(2), new Coordinates(1, 0));
+		vortexCannon.setOwner(players.get(0));
 	}
 
-	@Test
-	public void handlePrimaryFire() {
+	//@Test
+	public void primaryFireShooting_correctBehaviour() {
+		//Without optional effects.
+		QuestionContainer initialQuestion = vortexCannon.initialQuestion();
+		//Select standard fire.
+		QuestionContainer vortexCoordinates = vortexCannon.doActivationStep(0);
+		//Select vortex coordinates (1,1).
+		assertTrue(vortexCoordinates.getCoordinates().get(0).toString().equals("(1,1)"));
+		vortexCannon.doActivationStep(0);
 	}
 
-	@Test
-	public void primaryFire() {
-	}
 
-	@Test
-	public void getPrimaryTargets() {
-	}
-
-	@Test
-	public void handleOptionalEffect1() {
-	}
-
-	@Test
-	public void reset() {
-	}
-
-	@Test
-	public void canBeActivated() {
-	}
 }
