e5b3b6980327248704652cde97296415e62368f4
AndreaMarcer <marcer.andrea@gmail.com>
2019-06-19 22:42:36 +0200

    add all attributes to gameBoardController in gui

--- src/main/java/it/polimi/se2019/view/client/gui/GameBoardController.java
+++ src/main/java/it/polimi/se2019/view/client/gui/GameBoardController.java
@@ -8,73 +8,353 @@ import javafx.scene.image.ImageView;
 
 public class GameBoardController {
 	@FXML
+	private Group square00;
+	@FXML
 	private Button button00;
 	@FXML
-	private Button button01;
+	private ImageView ammoCard00;
 	@FXML
-	private Button button02;
+	private ImageView banshee00;
 	@FXML
-	private Button button03;
+	private ImageView sprog00;
 	@FXML
-	private Button button10;
+	private ImageView dozer00;
 	@FXML
-	private Button button11;
+	private ImageView destructor00;
 	@FXML
-	private Button button12;
-	@FXML
-	private Button button13;
-	@FXML
-	private Button button20;
-	@FXML
-	private Button button21;
-	@FXML
-	private Button button22;
-	@FXML
-	private Button button23;
+	private ImageView violet00;
 
-	@FXML
-	private Group square00;
+
 	@FXML
 	private Group square01;
 	@FXML
+	private Button button01;
+	@FXML
+	private ImageView ammoCard01;
+	@FXML
+	private ImageView banshee01;
+	@FXML
+	private ImageView sprog01;
+	@FXML
+	private ImageView dozer01;
+	@FXML
+	private ImageView destructor01;
+	@FXML
+	private ImageView violet01;
+
+	@FXML
 	private Group square02;
+	@FXML
+	private Button button02;
+	@FXML
+	private ImageView banshee02;
+	@FXML
+	private ImageView sprog02;
+	@FXML
+	private ImageView dozer02;
+	@FXML
+	private ImageView destructor02;
+	@FXML
+	private ImageView violet02;
+
 	@FXML
 	private Group square03;
 	@FXML
+	private Button button03;
+	@FXML
+	private ImageView ammoCard03;
+	@FXML
+	private ImageView banshee03;
+	@FXML
+	private ImageView sprog03;
+	@FXML
+	private ImageView dozer03;
+	@FXML
+	private ImageView destructor03;
+	@FXML
+	private ImageView violet03;
+
+	@FXML
 	private Group square10;
+	@FXML
+	private Button button10;
+	@FXML
+	private ImageView banshee10;
+	@FXML
+	private ImageView sprog10;
+	@FXML
+	private ImageView dozer10;
+	@FXML
+	private ImageView destructor10;
+	@FXML
+	private ImageView violet10;
+
 	@FXML
 	private Group square11;
 	@FXML
+	private Button button11;
+	@FXML
+	private ImageView ammoCard11;
+	@FXML
+	private ImageView banshee11;
+	@FXML
+	private ImageView sprog11;
+	@FXML
+	private ImageView dozer11;
+	@FXML
+	private ImageView destructor11;
+	@FXML
+	private ImageView violet11;
+
+	@FXML
 	private Group square12;
+	@FXML
+	private Button button12;
+	@FXML
+	private ImageView ammoCard12;
+	@FXML
+	private ImageView banshee12;
+	@FXML
+	private ImageView sprog12;
+	@FXML
+	private ImageView dozer12;
+	@FXML
+	private ImageView destructor12;
+	@FXML
+	private ImageView violet12;
+
+
 	@FXML
 	private Group square13;
 	@FXML
+	private Button button13;
+	@FXML
+	private ImageView ammoCard13;
+	@FXML
+	private ImageView banshee13;
+	@FXML
+	private ImageView sprog13;
+	@FXML
+	private ImageView dozer13;
+	@FXML
+	private ImageView destructor13;
+	@FXML
+	private ImageView violet13;
+
+	@FXML
 	private Group square20;
+	@FXML
+	private Button button20;
+	@FXML
+	private ImageView ammoCard20;
+	@FXML
+	private ImageView banshee20;
+	@FXML
+	private ImageView sprog20;
+	@FXML
+	private ImageView dozer20;
+	@FXML
+	private ImageView destructor20;
+	@FXML
+	private ImageView violet20;
+
 	@FXML
 	private Group square21;
 	@FXML
+	private Button button21;
+	@FXML
+	private ImageView ammoCard21;
+	@FXML
+	private ImageView banshee21;
+	@FXML
+	private ImageView sprog21;
+	@FXML
+	private ImageView dozer21;
+	@FXML
+	private ImageView destructor21;
+	@FXML
+	private ImageView violet21;
+
+	@FXML
 	private Group square22;
 	@FXML
+	private Button button22;
+	@FXML
+	private ImageView ammoCard22;
+	@FXML
+	private ImageView banshee22;
+	@FXML
+	private ImageView sprog22;
+	@FXML
+	private ImageView dozer22;
+	@FXML
+	private ImageView destructor22;
+	@FXML
+	private ImageView violet22;
+
+	@FXML
 	private Group square23;
+	@FXML
+	private Button button23;
+	@FXML
+	private ImageView banshee23;
+	@FXML
+	private ImageView sprog23;
+	@FXML
+	private ImageView dozer23;
+	@FXML
+	private ImageView destructor23;
+	@FXML
+	private ImageView violet23;
+
 
 	@FXML
 	private Button weaponRed0;
 	@FXML
+	private ImageView weponImageRed0;
+	@FXML
 	private Button weaponRed1;
+	@FXML
+	private ImageView weponImageRed1;
 	@FXML
 	private Button weaponRed2;
 	@FXML
+	private ImageView weponImageRed2;
+	@FXML
 	private Button weaponBlue0;
+	@FXML
+	private ImageView weponImageBlue0;
 	@FXML
 	private Button weaponBlue1;
 	@FXML
+	private ImageView weponImageBlue1;
+	@FXML
 	private Button weaponBlue2;
+	@FXML
+	private ImageView weponImageBlue2;
 	@FXML
 	private Button weaponYellow0;
 	@FXML
+	private ImageView weponImageYellow0;
+	@FXML
 	private Button weaponYellow1;
 	@FXML
+	private ImageView weponImageYellow1;
+	@FXML
 	private Button weaponYellow2;
+	@FXML
+	private ImageView weponImageYellow2;
+
+	@FXML
+	private ImageView playerIcon0;
+	@FXML
+	private Button playerIconButton0;
+	@FXML
+	private ImageView playerIcon1;
+	@FXML
+	private Button playerIconButton1;
+	@FXML
+	private ImageView playerIcon2;
+	@FXML
+	private Button playerIconButton2;
+	@FXML
+	private ImageView playerIcon3;
+	@FXML
+	private Button playerIconButton3;
+	@FXML
+	private ImageView playerIcon4;
+	@FXML
+	private Button playerIconButton4;
+
+	@FXML
+	private ImageView skullToken00;
+	@FXML
+	private ImageView skullToken10;
+	@FXML
+	private ImageView skullToken20;
+	@FXML
+	private ImageView skullToken30;
+	@FXML
+	private ImageView skullToken40;
+	@FXML
+	private ImageView skullToken50;
+	@FXML
+	private ImageView skullToken60;
+	@FXML
+	private ImageView skullToken70;
+	@FXML
+	private ImageView skullToken80;
+	@FXML
+	private ImageView skullToken90;
+	@FXML
+	private ImageView skullToken100;
+	@FXML
+	private ImageView skullToken110;
+	@FXML
+	private ImageView skullToken120;
+	@FXML
+	private ImageView skullToken130;
+	@FXML
+	private ImageView skullToken140;
+	@FXML
+	private ImageView skullToken150;
+	@FXML
+	private ImageView skullToken01;
+	@FXML
+	private ImageView skullToken11;
+	@FXML
+	private ImageView skullToken21;
+	@FXML
+	private ImageView skullToken31;
+	@FXML
+	private ImageView skullToken41;
+	@FXML
+	private ImageView skullToken51;
+	@FXML
+	private ImageView skullToken61;
+	@FXML
+	private ImageView skullToken71;
+	@FXML
+	private ImageView skullToken81;
+	@FXML
+	private ImageView skullToken91;
+	@FXML
+	private ImageView skullToken101;
+	@FXML
+	private ImageView skullToken111;
+	@FXML
+	private ImageView skullToken121;
+	@FXML
+	private ImageView skullToken131;
+	@FXML
+	private ImageView skullToken141;
+	@FXML
+	private ImageView skullToken151;
+
+	@FXML
+	private ImageView doubleKillToken0;
+	@FXML
+	private ImageView doubleKillToken1;
+	@FXML
+	private ImageView doubleKillToken2;
+	@FXML
+	private ImageView doubleKillToken3;
+	@FXML
+	private ImageView doubleKillToken4;
+	@FXML
+	private ImageView doubleKillToken5;
+	@FXML
+	private ImageView doubleKillToken6;
+	@FXML
+	private ImageView doubleKillToken7;
+	@FXML
+	private ImageView doubleKillToken8;
+	@FXML
+	private ImageView doubleKillToken9;
+	@FXML
+	private ImageView doubleKillToken10;
+	@FXML
+	private ImageView doubleKillToken11;
 
 	@FXML
 	private ImageView skull0;
@@ -95,6 +375,7 @@ public class GameBoardController {
 
 	@FXML
 	private ImageView backGround;
+
 
 	private Image loadImage(String filePath) {
 		return new Image(getClass().getResource("/graphicassets/" + filePath + ".png").toString());
