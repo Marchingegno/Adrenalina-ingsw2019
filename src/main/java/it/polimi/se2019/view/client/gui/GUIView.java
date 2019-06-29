97a4c0c33cdff16f6af7c562acdf2b209fbcdcb7
Desno365 <desno365@gmail.com>
2019-06-29 17:20:02 +0200

    Remove old Swing code

--- src/main/java/it/polimi/se2019/view/client/gui/GUIView.java
+++ src/main/java/it/polimi/se2019/view/client/gui/GUIView.java
@@ -19,7 +19,6 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import javax.swing.*;
-import java.awt.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -503,119 +502,6 @@ public class GUIView extends RemoteView {
 		Utils.logInfo("GUIView -> canAffordAlsoWithOnlyAmmo(): player ammo " + playerAmmo + " remaining price to pay " + priceToPay + " => player can afford");
 
 		return priceToPay.isEmpty();
-	}
-
-
-	// Not used, here just for example
-	// TODO remove
-	private void dialogWithCancel() {
-		Object[] possibleValues = {"RMI", "Socket"};
-		Object selectedValue = JOptionPane.showInputDialog(null,
-				"Choose one",
-				"Input",
-				JOptionPane.INFORMATION_MESSAGE,
-				null,
-				possibleValues,
-				possibleValues[0]);
-		if (selectedValue == possibleValues[0])
-			startConnectionWithRMI();
-		else if (selectedValue == possibleValues[1])
-			startConnectionWithSocket();
-		else if (selectedValue == null)
-			closeProgram();
-	}
-
-	private void showWaitingRoomFrame() {
-		// Create Frame.
-		JFrame.setDefaultLookAndFeelDecorated(true);
-		waitingRoomFrame = new JFrame("Waiting Room");
-		waitingRoomFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Set behaviour on close.
-
-		// Create Panel.
-		JPanel panel = new JPanel();
-		GridLayout gridLayout = new GridLayout(0, 1); // With 0 rows is like a vertical layout.
-		gridLayout.setHgap(20);
-		panel.setLayout(gridLayout);
-
-		JLabel introductionText = new JLabel("Waiting for other players to connect.");
-		introductionText.setOpaque(true);
-		panel.add(introductionText);
-
-		waitingPlayersTextWaitingRoom = new JLabel("Players in the waiting room: .");
-		waitingPlayersTextWaitingRoom.setOpaque(true);
-		panel.add(waitingPlayersTextWaitingRoom);
-
-		timerTextWaitingRoom = new JLabel(" ");
-		timerTextWaitingRoom.setOpaque(true);
-		panel.add(timerTextWaitingRoom);
-
-		waitingRoomFrame.add(panel);
-		waitingRoomFrame.pack(); // Sizes the frame so that all its contents are at or above their preferred sizes.
-		waitingRoomFrame.setVisible(true); // Set the window to be visible.
-	}
-
-	private void closeWaitingRoomFrame() {
-		if (waitingRoomFrame != null) {
-			waitingRoomFrame.setVisible(false);
-			waitingRoomFrame.dispose();
-		}
-	}
-
-	private void showGameConfigFrame() {
-		// Create options.
-		String[] skullsOptions = new String[GameConstants.MAX_SKULLS - GameConstants.MIN_SKULLS + 1];
-		for (int i = GameConstants.MIN_SKULLS; i <= GameConstants.MAX_SKULLS; i++)
-			skullsOptions[i - GameConstants.MIN_SKULLS] = ((Integer) i).toString();
-		String[] mapOptions = new String[GameConstants.MapType.values().length];
-		for (int i = 0; i < GameConstants.MapType.values().length; i++) {
-			mapOptions[i] = GameConstants.MapType.values()[i].getDescription();
-		}
-
-		// Create frame.
-		gameConfigFrame = new JFrame("Game Configurations");
-		gameConfigFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		// Up panel.
-		final JPanel upPanel = new JPanel();
-		GridLayout upGridLayout = new GridLayout(1, 1);
-		upPanel.setLayout(upGridLayout);
-		JLabel introductionText = new JLabel("Select the map and how many skulls you would like to use.");
-		upPanel.add(introductionText);
-
-		// Down panel.
-		JPanel downPanel = new JPanel();
-		downPanel.setLayout(new GridLayout(2, 3));
-		downPanel.add(new JLabel("Skulls:"));
-		downPanel.add(new JLabel("Map:"));
-		downPanel.add(new JLabel(" "));
-		JComboBox skullsComboBox = new JComboBox<>(skullsOptions);
-		downPanel.add(skullsComboBox);
-		JComboBox mapComboBox = new JComboBox<>(mapOptions);
-		mapComboBox.setPreferredSize(skullsComboBox.getPreferredSize()); // Make the same dimension as the skulls options.
-		downPanel.add(mapComboBox);
-		JButton doneButton = new JButton("Done");
-		doneButton.addActionListener(e -> {
-			closeGameConfigFrame();
-			showGameConfigWaiting();
-
-			int skulls = skullsComboBox.getSelectedIndex() + GameConstants.MIN_SKULLS;
-			int mapIndex = mapComboBox.getSelectedIndex();
-
-			GameConfigMessage gameConfigMessage = new GameConfigMessage(MessageSubtype.ANSWER);
-			gameConfigMessage.setMapIndex(mapIndex);
-			gameConfigMessage.setSkulls(skulls);
-			sendMessage(gameConfigMessage);
-		});
-		downPanel.add(doneButton);
-
-		// Add panels to container panel.
-		gameConfigFrame.getContentPane().add(upPanel, BorderLayout.NORTH);
-		gameConfigFrame.getContentPane().add(new JSeparator(), BorderLayout.CENTER);
-		gameConfigFrame.getContentPane().add(downPanel, BorderLayout.SOUTH);
-
-		// Display the window.
-		gameConfigFrame.pack();
-		gameConfigFrame.setVisible(true);
 	}
 
 	private void closeGameConfigFrame() {
