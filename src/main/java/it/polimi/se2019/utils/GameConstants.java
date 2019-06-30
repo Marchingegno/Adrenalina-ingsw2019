9667d3975713f113b30d97130041587145144b0a
Matteo Marchisciana <24322895+Marchingegno@users.noreply.github.com>
2019-06-30 10:29:43 +0200

    Restore death and overkill thresholds to standard value

--- src/main/java/it/polimi/se2019/utils/GameConstants.java
+++ src/main/java/it/polimi/se2019/utils/GameConstants.java
@@ -9,8 +9,8 @@ package it.polimi.se2019.utils;
 public class GameConstants {
 
 	public static final int MAX_PLAYERS = 5;
-	public static final int DEATH_DAMAGE = 2;
-	public static final int OVERKILL_DAMAGE = 3;
+	public static final int DEATH_DAMAGE = 11;
+	public static final int OVERKILL_DAMAGE = 12;
 	public static final int MAX_MARKS_PER_PLAYER = 3;
 	public static final int MAX_WEAPON_CARDS_PER_PLAYER = 3;
 	public static final int MAX_POWERUP_CARDS_PER_PLAYER = 3;
